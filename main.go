1910f7dfb03071b0688f3705e43da2c5c99d86a2
mateuszwicherkski <wicherskimateusz@gmail.com>
2022-12-10 15:28:58 +0100

    Basic window

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/faiface/pixel/pixelgl"
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func createWindow() *pixelgl.Window {
+	cfg :=pixelgl.WindowConfig{
+		Title: "Brick Breaker",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync: false,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	return win
+}
 
 func run() {
-	
+	win := createWindow()
+
+	for !win.Closed() {
+		win.Update()
+	}
 }
 
 func main() {
