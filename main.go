4df634180e7c47101c72ef673190c1cc9c90ba0a
mateuszwicherkski <wicherskimateusz@gmail.com>
2022-12-10 17:12:51 +0100

    Drawing a ball

--- main.go
+++ main.go
@@ -20,6 +20,20 @@ func (b *block) draw(imd *imdraw.IMDraw) {
 	imd.Rectangle(0)
 }
 
+type ball struct {
+	rect pixel.Circle
+	color color.Color
+	x float64
+	y float64
+	vel float64
+}
+
+func (b *ball) draw(imd *imdraw.IMDraw) {
+	imd.Color = b.color
+	imd.Push(pixel.V(b.x, b.y))
+	imd.Circle(15, 0)
+}
+
 func createWindow() *pixelgl.Window {
 	cfg :=pixelgl.WindowConfig{
 		Title: "Brick Breaker",
@@ -42,12 +56,20 @@ func run() {
 		rect: pixel.R(5, win.Bounds().H() -100, 105, win.Bounds().H() -50),
 	}
 
+	test_ball := ball {
+		color: colornames.Green,
+		rect: pixel.C(pixel.V(win.Bounds().W(), win.Bounds().H()), 15),
+		x: 300,
+		y: 300,
+	}
+
 	imd := imdraw.New(nil)
 	imd.Precision = 32
 
 	for !win.Closed() {
 		win.Clear(colornames.Darkslateblue)
 		test_block.draw(imd)
+		test_ball.draw(imd)
 		
 		imd.Draw(win)
 		win.Update()
