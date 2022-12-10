21c958e95bf6607a9b5213b5c996d1b19cdbe49f
mateuszwicherkski <wicherskimateusz@gmail.com>
2022-12-10 16:27:12 +0100

    Drawing a Block

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
+
+type block struct {
+	rect pixel.Rect
+	color color.Color
+}
+
+func (b *block) draw(imd *imdraw.IMDraw) {
+	imd.Color = b.color
+	imd.Push(b.rect.Min, b.rect.Max)
+	imd.Rectangle(0)
+}
 
 func createWindow() *pixelgl.Window {
 	cfg :=pixelgl.WindowConfig{
@@ -22,8 +36,20 @@ func createWindow() *pixelgl.Window {
 func run() {
 	win := createWindow()
 
+	// Create stuff
+	test_block := block {
+		color: colornames.Red,
+		rect: pixel.R(5, win.Bounds().H() -100, 105, win.Bounds().H() -50),
+	}
+
+	imd := imdraw.New(nil)
+	imd.Precision = 32
+
 	for !win.Closed() {
 		win.Clear(colornames.Darkslateblue)
+		test_block.draw(imd)
+		
+		imd.Draw(win)
 		win.Update()
 	}
 	
